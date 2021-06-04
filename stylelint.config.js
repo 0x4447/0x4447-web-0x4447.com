338d5767df8d939f2e65312c053e71715869e39b
sosog <sosogvritishvilI@gmail.com>
2021-06-05 00:15:11 +0400

    eslint erros fixing is finished

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,25 +1,25 @@
 module.exports = {
-  extends: ["stylelint-config-standard", "stylelint-config-prettier"],
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
   rules: {
-    "at-rule-no-unknown": [
+    'at-rule-no-unknown': [
       true,
       {
         ignoreAtRules: [
-          "function",
-          "if",
-          "each",
-          "for",
-          "include",
-          "mixin",
-          "return",
-          "extend",
-          "use",
-          "forward",
+          'function',
+          'if',
+          'each',
+          'for',
+          'include',
+          'mixin',
+          'return',
+          'extend',
+          'use',
+          'forward',
         ],
       },
     ],
-    "unit-allowed-list": ["em", "rem", "%", "px", "vw", "vh", "s", "deg", "fr"],
+    'unit-allowed-list': ['em', 'rem', '%', 'px', 'vw', 'vh', 's', 'deg', 'fr'],
   },
-};
+}
